da2717a10b8b0f09aee5196fe59cf08454222f80
Greg Taylor <gregoryptaylor@gmail.com>
2017-06-25 08:30:27 -0700

    Simplified Code for 2 Sensors
    Simplified the code, particularly for defining the field variables
    which are used to build the Thingspeak Json. they are not discretely
    defined instead of being generated by a for loop, which was
    restrictive.

--- src/DIVIS_Collector.ino
+++ src/DIVIS_Collector.ino
@@ -2,8 +2,9 @@
 // DIVIS VERSION NOTES
 //========================================
 
-// v 009
-// adding coeficients for sensors in order to lower error readings
+// v 010
+// adding additional metrics which measure and average differences between sensor readings and report them to a second thingspeak channel
+// removed functions that are obsolete
 
 //========================================
 // Test Status: Not tested
@@ -133,48 +134,51 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS3472
 // gain options; 1, 4, 16, 60
 
 //*****************Configuration Variables***********************
-#define g_numDevice 8 // set number of TCS devices
+#define g_numDevice 2 // set number of TCS devices. Max = 7.
 #define g_sample 150 // set number of samples per cycle
 #define sleepHour 21 // set 24 hour time to start night sleep
 #define wakeHour 6 // set 24 hour time to wake
-#define restTime 240 // set time to rest between cycles in seconds
+#define restTime 60 // set time to rest between cycles in seconds
+#define sat_max 10240 // set the maximum value returned by the sensor for saturation calculations
+String DIVIS_ID = "001";
 //*********************Sensor Corretion Coefficients*************
-float r_coef[] = {1.0,1.0,1.0,1.0,1.0,1.0};
-float g_coef[] = {1.0,1.0,1.0,1.0,1.0,1.0};
-float b_coef[] = {1.0,1.0,1.0,1.0,1.0,1.0};
-float c_coef[] = {1.0,1.0,1.0,1.0,1.0,1.0};
+// must adjust depeding on number of sensors
+float r_coef[] = {1.0,0.97096181};
+float g_coef[] = {1.0,0.989730857};
+float b_coef[] = {1.0,1.001603333};
+float c_coef[] = {1.0,0.984265143};
 
-/* // Values from first full day of testing with 8 sensors
-float r_coef[] = {1.0814,0.9905,1.072,1.0217,1.0477,1.0237};
-float g_coef[] = {1.0162,0.9667,0.9429,0.9502,0.9992,1.0094};
-float b_coef[] = {1.0103,0.9552,0.9584,0.9673,0.9823,0.991};
-float c_coef[] = {1.0349,0.9901,1.0383,1.0155,1.0046,1.0148};
-*/
+
 //***************************************************************
 
 uint16_t clear, red, green, blue;
-String sensorReadings[g_numDevice];
-
-
-
-//Variables for writing to Thingspeak
+String sensor_raw[g_numDevice];
+String sensor_diff[g_numDevice];
 String TSjson;
-String api_key = "VLCV4WYEJY2V5WAU"; // Replace this string with a valid ThingSpeak Write API Key.
-String field1 = "24ms/1x";
-String field2 = "";  // i.e. field2 is null
-String field3 = "";
-String field4 = "";
-String field5 = "";
-String field6 = "";
-String field7 = "";
-String field8 = "";
+String api_key = "CCDMJAB229VPBB5O"; // api key for red and green data
+
+//variables to count saturation for each color reading
+
+//Variables for writing to raw channel
+
+String field1 = "1"; // DIVIS ID
+String field2 = "";  // Upper sensor R,B,G,C
+String field3 = "";	 // Lower sensor R,B,G,C
+String field4 = "";	 // Saturation ratio R,G,B,C
+String field5 = "";  // r difference ratio
+String field6 = "";	 // g difference ratio
+String field7 = "";	 // b difference ratio
+String field8 = "";  // c difference ratio
 String lat = "";
 String lon = "";
 String el = "";
 String status = "";
 
+
+//Variables for writing to raw channel
+
 //************************************************
-//************************************************
+//************* M A I N   L O O P ****************
 //************************************************
 
 void setup() {
@@ -189,8 +193,8 @@ void loop() {
 
 		nightTime(); // turns off at nighttime
 		sampleMulti(g_numDevice,g_sample,1,1); //take samples from all devices saves the to an array which is concatenated into a string
-		createTSjson(TSjson,g_numDevice); // creates the json string to pass to thingspeak
-		publishToThingSpeak(); // publishes data to thingspeak
+		createTSjson(TSjson); // creates the json string to pass to thingspeak
+		publishToThingSpeak(TSjson); // publishes raw data to thingspeak
 		delay(restTime * 1000); // simulating being in sleep mode
 		//System.sleep(SLEEP_MODE_DEEP, restTime);
     }
@@ -199,7 +203,7 @@ void loop() {
 //************************************************
 //************************************************
 
-
+// INITIALIZATION
 void initializeTCS(int device) {
 
    mux.setChannel(device);
@@ -225,7 +229,9 @@ void initializeMultiTCS(int numDevice){
 	}
 	Particle.publish("All TCS34725 Found!",NULL,60,PRIVATE);
 }
+//======================================================
 
+//DATA COLLECTION
 // collects a single data point from one TCS34725 sensor and saves to global red/green/blue/clear variables
 void collect(int device, bool printbool){
 	mux.setChannel(device);
@@ -288,17 +294,39 @@ if(printbool==1){
 // creates aggregate data in an array then overrights that data with the mean
 void sampleMulti(int numDevice, int samples, bool printbool, bool multiprintbool){
 	 Particle.publish("Start Data Collection",NULL,60,PRIVATE);
+
+	 // delcare variables for storing sample data and making calucations
 	 unsigned long start = millis();
 	 float runtime = 0;
+	 //variables for initial readings
 	 float r [numDevice];
 	 float g [numDevice];
 	 float b [numDevice];
 	 float c [numDevice];
+	 //variables for raw readings
 	 int r_int [numDevice];
 	 int g_int [numDevice];
 	 int b_int [numDevice];
 	 int c_int [numDevice];
+	 //variables for calculated differences
+	 float r_diff [numDevice];
+	 float g_diff [numDevice];
+	 float b_diff [numDevice];
+	 float c_diff [numDevice];
+	 //variables for saturation
+	 int r_sat_counter = 0;
+	 int g_sat_counter = 0;
+	 int b_sat_counter = 0;
+	 int c_sat_counter = 0;
 
+	 int r_sat_ratio = 0;
+	 int g_sat_ratio = 0;
+	 int b_sat_ratio = 0;
+	 int c_sat_ratio = 0;
+
+
+
+// Take N samples from each device and create a running total. each device is polled in tern, in order to make observations between devices as close together in time as possible
 		for (int i=0; i<samples; i++){
 				for (int i=0; i<numDevice; i++) {
 					collect(i, printbool);
@@ -306,22 +334,61 @@ void sampleMulti(int numDevice, int samples, bool printbool, bool multiprintbool
 					b[i] = b[i] + (blue * b_coef[i]);
 					g[i] = g[i] + (green * g_coef[i]);
 					c[i] = c[i] + (clear * c_coef[i]);
+
+					// count readings with saturation on the upper sensor
+					if ((i==0) && (red > (sat_max * 0.7))){
+						r_sat_counter = r_sat_counter + 1;
+					}
+					if ((i==0) && (green > (sat_max * 0.7))){
+						g_sat_counter = g_sat_counter + 1;
+					}
+					if ((i==0) && (blue > (sat_max * 0.7))){
+						b_sat_counter = b_sat_counter + 1;
+					}
+					if ((i==0) && (clear > (sat_max * 0.7))){
+						c_sat_counter = c_sat_counter + 1;
+					}
+
 				}
 			}
 
+
+for(int i=0; i<(numDevice-1); i++){
+	r_diff[i] = (r[i] - r[i+1])/r[i];
+	g_diff[i] = (g[i] - g[i+1])/g[i];
+	b_diff[i] = (b[i] - b[i+1])/b[i];
+	c_diff[i] = (c[i] - c[i+1])/c[i];
+}
+
+
+// Calculate final values for raw data
 for (int i=0; i<numDevice; i++){
 		r_int[i] = (int)round(r[i] / samples);
 		b_int[i] = (int)round(b[i] / samples);
 		g_int[i] = (int)round(g[i] / samples);
 		c_int[i] = (int)round(c[i] / samples);
-
 	}
 
-//convert numbers to string and save in global variable to prepare to send to
-	for (int i=0; i<numDevice; i++){
-			sensorReadings[i] = String(r_int[i]) + ";" + String(g_int[i]) + ";" +  String(b_int[i]) + ";" +  String(c_int[i]);
-		}
+// saturation of upper sensor
+		r_sat_ratio = (r_sat_counter/samples)*100;
+		g_sat_ratio = (g_sat_counter/samples)*100;
+		b_sat_ratio = (b_sat_counter/samples)*100;
+		c_sat_ratio = (c_sat_counter/samples)*100;
 
+// Map readings to sensor data
+
+		field1 = DIVIS_ID; // DIVIS ID
+		field2 = String(r_int[0]) + "," + String(g_int[0]) + "," + String(b_int[0]) + "," + String(c_int[0]);  // Upper sensor R,B,G,C
+		field3 = String(r_int[1]) + "," + String(g_int[1]) + "," + String(b_int[1]) + "," + String(c_int[1]);	 // Lower sensor R,B,G,C
+		field4 = String(r_sat_ratio) + "," + String(g_sat_ratio) + "," + String(b_sat_ratio) + "," + String(c_sat_ratio);  // Saturation ratio R,G,B,C
+		field5 = String(r_diff[0]);	 // r difference ratio
+		field6 = String(g_diff[0]);  // g difference ratio
+		field7 = String(b_diff[0]);	 // b difference ratio
+		field8 = String(c_diff[0]);	 // c difference ratio
+		lat = "";
+		lon = "";
+		el = "";
+		status = "";
 
 
 		runtime = (millis() - start)/1000;
@@ -333,66 +400,72 @@ if (multiprintbool == 1) {
 			Serial.print("device:");
 			Serial.print(i);
 			Serial.print("::");
-			Serial.print(r[i]);
+			Serial.print(r_int[i]);
 			Serial.print("|");
-			Serial.print(b[i]);
+			Serial.print(b_int[i]);
 			Serial.print("|");
-			Serial.print(g[i]);
+			Serial.print(g_int[i]);
 			Serial.print("|");
-			Serial.println(c[i]);
+			Serial.println(c_int[i]);
 		}
 	}
 }
+//======================================================
 
-void publishToThingSpeak(){
-	Particle.publish("divisWrite_", TSjson, 60, PRIVATE);
-	Particle.publish("divisString",String(TSjson.length()),60,PRIVATE);
+
+//PUBLISH
+
+//publish to thingspeak
+void publishToThingSpeak(String json){
+	Particle.publish("divisWrite_", json, 60, PRIVATE);
+	Particle.publish("divisString",String(json.length()),60,PRIVATE);
 	Serial.println("publishing to Thingspeak...");
-	//delay(20000); //ensures minimum time between writes to thingspeak.
+  delay(15000); //ensures minimum time between writes to thingspeak.
 	//Make sure to delete this delay once the device is put into deep sleep between readings
 }
 
-void nightTime(){
-// checks for day light savings time and sets the time zone offset from UTC
-	if(Time.isDST() == 1){
-
-		Time.zone(-8); // sets timezone to PST w/out daylight savings
-	}
-	else{
-		Time.zone(-7); // sets timezone to PST w/ daylight savings
-	}
-	int currentHour = Time.hour();
-	Particle.publish("hour_", "CurrentHour: " + String(currentHour) + " / " + "IsDST: " + String(Time.isDST()), 60, PRIVATE);
-
-	if (currentHour >= sleepHour){
-		int sleepHours = (24 - sleepHour + wakeHour); // calculates sleep time from sleephour and wake hour
-		int sleepSeconds = sleepHours * 60 * 60; // converts sleep and wake hours to a sleep time in seconds
-		Particle.publish("going to sleep",String(sleepSeconds),60,PRIVATE);
-		delay(5000);
-		System.sleep(SLEEP_MODE_DEEP, sleepSeconds);
-	}
-}
-
-//****************************WebHook Notes and Functions**************************************************
+//Create Thingspeak Json
 //This code was modified from the following sources
 //Source1: https://www.hackster.io/15223/thingspeak-particle-photon-using-webhooks-dbd96c
 //Source2: https://github.com/mawrob/thingspeak-particle-webhookwrite/blob/master/source.ino
 
 
-// Demo program that uses a Particle Webhook to POST data to ThingSpeak.
-// For details on the ThingSpeak Channel Feed api see: https://www.mathworks.com/help/thingspeak/update-channel-feed.html
-
-void createTSjson(String &dest, int numDevice) {
+void createTSjson(String &dest) {
 
 
 		dest = "{";
 
 
-		for (int i=0; i<numDevice; i++){
-			int field=i+1;
-			if(sensorReadings[i].length()>0){
-					dest = dest + "\""+ field +"\":\""+ sensorReadings[i] +"\",";
-			}
+		if(field1.length()>0){
+					dest = dest + "\"1\":\""+ field1 +"\",";
+		}
+
+		if(field2.length()>0){
+					dest = dest + "\"2\":\""+ field2 +"\",";
+		}
+
+		if(field3.length()>0){
+					dest = dest + "\"3\":\""+ field3 +"\",";
+		}
+
+		if(field4.length()>0){
+					dest = dest + "\"4\":\""+ field4 +"\",";
+		}
+
+		if(field5.length()>0){
+					dest = dest + "\"5\":\""+ field5 +"\",";
+		}
+
+		if(field6.length()>0){
+					dest = dest + "\"6\":\""+ field6 +"\",";
+		}
+
+		if(field7.length()>0){
+					dest = dest + "\"7\":\""+ field7 +"\",";
+		}
+
+		if(field8.length()>0){
+					dest = dest + "\"8\":\""+ field8 +"\",";
 		}
 
     if(lat.length()>0){
@@ -413,4 +486,30 @@ void createTSjson(String &dest, int numDevice) {
 
     dest = dest + "\"k\":\"" + api_key + "\"}";
 
+		Serial.println(TSjson);
+
 }
+//=========================================================
+
+//SLEEP CONTROLL
+void nightTime(){
+// checks for day light savings time and sets the time zone offset from UTC
+	if(Time.isDST() == 1){
+
+		Time.zone(-8); // sets timezone to PST w/out daylight savings
+	}
+	else{
+		Time.zone(-7); // sets timezone to PST w/ daylight savings
+	}
+	int currentHour = Time.hour();
+	Particle.publish("hour_", "CurrentHour: " + String(currentHour) + " / " + "IsDST: " + String(Time.isDST()), 60, PRIVATE);
+
+	if (currentHour >= sleepHour){
+		int sleepHours = (24 - sleepHour + wakeHour); // calculates sleep time from sleephour and wake hour
+		int sleepSeconds = sleepHours * 60 * 60; // converts sleep and wake hours to a sleep time in seconds
+		Particle.publish("going to sleep",String(sleepSeconds),60,PRIVATE);
+		delay(5000);
+		System.sleep(SLEEP_MODE_DEEP, sleepSeconds);
+	}
+}
+//===============================================================
